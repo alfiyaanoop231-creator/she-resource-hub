13b8f13077db6c5095dc2b4c996fbba0c44ed351
alfiyaanoop231-creator <alfiyaanoop231@gmail.com>
2026-02-28 11:57:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function toggleMode() {
-    document.body.classList.toggle("dark-mode");
-}
-
 function showMessage() {
     alert("Your request has been submitted safely 💜");
+}
+
+function showEmergency() {
+    alert("Call 181 (Women Helpline) immediately!");
+}
+
+function searchContent() {
+    let input = document.getElementById("search").value.toLowerCase();
+    let content = document.querySelector(".content");
+    if (content.innerText.toLowerCase().includes(input)) {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
 }
